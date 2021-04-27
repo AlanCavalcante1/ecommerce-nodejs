25bb894f18ce6d2a04d4c0e48aaf2a56b30d2099
Alan Cavalcante <alanlimaemail@gmail.com>
2021-04-27 14:43:41 -0300

    change app to use express.Router()

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express();
+const router = express.Router();
 const userController = require('../controller/userController');
 const authMiddleware = require('../middlewares/auth');
 //router.use(authMiddleware)*/
